0cfdffb73a78b99f0e90723adfc30da9a950e9ef
LeeNaYoung240 <ajd092710@gmail.com>
2025-05-07 21:40:09 +0900

    first commit

---
+++ src/test/java/com/todayter/TodayterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
