1b1c1fd9842bf098eece17b9cf5465a56ba22f58
LeeNaYoung240 <ajd092710@gmail.com>
2025-07-16 04:31:36 +0900

    [#45] 🩹 fix : @Value에서 cloud.aws.credentials.accessKey 대소문자 오류 수정

--- src/main/java/com/todayter/global/config/S3Config.java
+++ src/main/java/com/todayter/global/config/S3Config.java
@@ -2,7 +2,7 @@ package com.todayter.global.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,18 +11,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.accesskey}")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
-    @Value("${cloud.aws.credentials.secretkey}")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 amazonS3() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
